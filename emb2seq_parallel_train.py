df8c4939c20da7d5f928d8de66f746a8fbcda4cb
cristianoBY <boyu2844240635@gmail.com>
2019-06-17 16:19:00 -0400

    decoder parallel done; SemEval dev done

--- emb2seq_parallel_train.py
+++ emb2seq_parallel_train.py
@@ -45,8 +45,8 @@ from emb2seq_model import *
 
 # get the decoder vocab
 with open('./data/vocab.pkl', 'rb') as f:
-    vocab = pickle.load(f)
-    print("Size of vocab: {}".format(vocab.idx))
+	vocab = pickle.load(f)
+	print("Size of vocab: {}".format(vocab.idx))
 
 
 # In[4]:
@@ -78,10 +78,10 @@ optimizer = optim.Adam(emb2seq_model.parameters())
 
 # randomly initialize the weights
 def init_weights(m):
-    for name, param in m.named_parameters():
-        if param.requires_grad:
-            print(name, param.shape)
-        # nn.init.uniform_(param.data, -0.08, 0.08)   
+	for name, param in m.named_parameters():
+		if param.requires_grad:
+			print(name, param.shape)
+		# nn.init.uniform_(param.data, -0.08, 0.08)   
 emb2seq_model.apply(init_weights)
 
 # ignore idx on the padding
@@ -96,33 +96,33 @@ criterion = nn.CrossEntropyLoss(ignore_index = PAD_IDX).to(device)
 # utility function
 # turn the given definition into its index list form
 def def2idx(definition, max_length, vocab):
-    
-    # definition is given by the WN NLTK API in a string
-    def_tokens = nltk.tokenize.word_tokenize(definition.lower())
-    
-    # limit the length if too long, trim
-    if len(def_tokens) > (max_length - 2):
-        def_tokens = def_tokens[0:(max_length - 2)]
-        
-        # add the start and end symbol
-        def_tokens = ['<start>'] + def_tokens + ['<end>']
-    
-    # if the length is too short, pad
-    elif len(def_tokens) < (max_length - 2):
-        
-        # add the start and end symbol
-        def_tokens = ['<start>'] + def_tokens + ['<end>']
-        
-        pad = ['<pad>'] * (max_length - len(def_tokens))
-        def_tokens = def_tokens + pad
-        
-    else:
-        def_tokens = ['<start>'] + def_tokens + ['<end>']
-            
-    # get the index for each element in the token list
-    def_idx_list = [vocab(token) for token in def_tokens]
-    
-    return def_idx_list
+	
+	# definition is given by the WN NLTK API in a string
+	def_tokens = nltk.tokenize.word_tokenize(definition.lower())
+	
+	# limit the length if too long, trim
+	if len(def_tokens) > (max_length - 2):
+		def_tokens = def_tokens[0:(max_length - 2)]
+		
+		# add the start and end symbol
+		def_tokens = ['<start>'] + def_tokens + ['<end>']
+	
+	# if the length is too short, pad
+	elif len(def_tokens) < (max_length - 2):
+		
+		# add the start and end symbol
+		def_tokens = ['<start>'] + def_tokens + ['<end>']
+		
+		pad = ['<pad>'] * (max_length - len(def_tokens))
+		def_tokens = def_tokens + pad
+		
+	else:
+		def_tokens = ['<start>'] + def_tokens + ['<end>']
+			
+	# get the index for each element in the token list
+	def_idx_list = [vocab(token) for token in def_tokens]
+	
+	return def_idx_list
   
 
 
@@ -131,16 +131,29 @@ def def2idx(definition, max_length, vocab):
 
 # utility function
 # get the literal and indices of the definition of the tagged word
-def get_def(instance):
-    key = ''
-    target_file = open("../WSD_Evaluation_Framework/Training_Corpora/SemCor/semcor.gold.key.txt", "r")
-    for line in target_file:
-        if line.startswith(instance.get('id')):
-            key = line.replace('\n', '').split(' ')[-1]
-            
-    # literal definition from the WN
-    definition = wn.lemma_from_key(key).synset().definition()
-    return definition
+# SemCor for train
+def get_SemCor_def(instance):
+	key = ''
+	target_file = open("../WSD_Evaluation_Framework/Training_Corpora/SemCor/semcor.gold.key.txt", "r")
+	for line in target_file:
+		if line.startswith(instance.get('id')):
+			key = line.replace('\n', '').split(' ')[-1]
+			
+	# literal definition from the WN
+	definition = wn.lemma_from_key(key).synset().definition()
+	return definition
+
+# SemEval 2007 for dev
+def get_SemEval_def(instance):
+	key = ''
+	target_file = open("../WSD_Evaluation_Framework/Evaluation_Datasets/semeval2007/semeval2007.gold.key.txt", "r")
+	for line in target_file:
+		if line.startswith(instance.get('id')):
+			key = line.replace('\n', '').split(' ')[-1]
+			
+	# literal definition from the WN
+	definition = wn.lemma_from_key(key).synset().definition()
+	return definition
 
 
 # In[8]:
@@ -148,12 +161,16 @@ def get_def(instance):
 
 # parse the SemCor training data
 import xml.etree.ElementTree as ET
-tree = ET.parse('../WSD_Evaluation_Framework/Training_Corpora/SemCor/semcor.data.xml')
-corpus = tree.getroot()
+semcor_tree = ET.parse('../WSD_Evaluation_Framework/Training_Corpora/SemCor/semcor.data.xml')
+semcor_corpus = semcor_tree.getroot()
 
-# small sets of SemCor
-small_train_size = 150
-small_dev_size = 170
+# parse the SemEval dev data
+semeval_tree = ET.parse('../WSD_Evaluation_Framework/Evaluation_Datasets/semeval2007/semeval2007.data.xml')
+semeval_corpus = semeval_tree.getroot()
+
+# small train and test sets
+small_train_size = 1
+small_dev_size = 1
 
 
 # In[9]:
@@ -161,65 +178,65 @@ small_dev_size = 170
 
 # the training function
 def train(model, optimizer, corpus, criterion, clip):
-    
-    model.train()
-    epoch_loss = 0
-    sentence_num = 0
-    
-    for sub_corpus in corpus[0:small_train_size]:
-    
-        for sent in sub_corpus:
+	
+	model.train()
+	epoch_loss = 0
+	sentence_num = 0
+	
+	for sub_corpus in corpus[0:small_train_size]:
+	
+		for sent in sub_corpus:
 
-            optimizer.zero_grad()
-            
-            # get the plain text sentence
-            sentence = [word.text for word in sent]
-            
-            # get the tagged ambiguous words
-            tagged_sent = [instance for instance in sent if instance.tag == 'instance']
-            # print(sentence)
-            # print(tagged_sent)
-            
-            # only use sentence with at least one tagged word
-            if len(tagged_sent) > 0:
-                
-                sentence_num += 1
-                
-                # get all-word definitions, batch_size is the sentence length
-                # [batch_size, self.max_length]
-                definitions = []
-                for instance in tagged_sent:
-                    
-                    # get the sense from the target file with ID
-                    definition = get_def(instance)
-                    def_idx_list = def2idx(definition, model.max_length, vocab)
-                    definitions.append(def_idx_list)
+			optimizer.zero_grad()
+			
+			# get the plain text sentence
+			sentence = [word.text for word in sent]
+			
+			# get the tagged ambiguous words
+			tagged_sent = [instance for instance in sent if instance.tag == 'instance']
+			# print(sentence)
+			# print(tagged_sent)
+			
+			# only use sentence with at least one tagged word
+			if len(tagged_sent) > 0:
+				
+				sentence_num += 1
+				
+				# get all-word definitions, batch_size is the sentence length
+				# [batch_size, self.max_length]
+				definitions = []
+				for instance in tagged_sent:
+					
+					# get the sense from the target file with ID
+					definition = get_SemCor_def(instance)
+					def_idx_list = def2idx(definition, model.max_length, vocab)
+					definitions.append(def_idx_list)
 
-                # get the encoder-decoder result
-                # (self.max_length, batch_size, vocab_size)
-                output, _ = model(sentence, tagged_sent, definitions, teacher_forcing_ratio = 0.4)
-                
-                # adjust dimension for loss calculation
-                # (self.max_length * batch_size, vocab_size)
-                output = output.view(-1, output.shape[-1])
-                target = torch.tensor(definitions, dtype = torch.long).to(device)
+				# get the encoder-decoder result
+				# (self.max_length, batch_size, vocab_size)
+				output, _ = model(sentence, tagged_sent, definitions, teacher_forcing_ratio = 0.4)
+				
+				# adjust dimension for loss calculation
+				# (self.max_length * batch_size, vocab_size)
+				output = output.view(-1, output.shape[-1])
+				target = torch.tensor(definitions, dtype = torch.long).to(device)
 
-                # (self.max_length * batch_size)
-                target = torch.transpose(target, 0, 1).contiguous().view(-1)
-                '''
-                output = output.permute(1, 2, 0)
-                target = torch.tensor(definitions, dtype = torch.long).to(device)
-                '''
-                loss = criterion(output, target)
-                loss.backward()
+				# (self.max_length * batch_size)
+				target = torch.transpose(target, 0, 1).contiguous().view(-1)
+				'''
+				output = output.permute(1, 2, 0)
+				target = torch.tensor(definitions, dtype = torch.long).to(device)
+				'''
+				loss = criterion(output, target)
+				loss.backward()
 
-                # add clip for gradient boost
-                # torch.nn.utils.clip_grad_norm_(model.parameters(), clip)
+				# add clip for gradient boost
+				# torch.nn.utils.clip_grad_norm_(model.parameters(), clip)
 
-                optimizer.step()
-                epoch_loss += loss.item()
-                
-    return epoch_loss / sentence_num
+				optimizer.step()
+				epoch_loss += loss.item()
+				
+	return epoch_loss / sentence_num
 
 
 # In[10]:
@@ -227,64 +244,64 @@ def train(model, optimizer, corpus, criterion, clip):
 
 # evaluate the model
 def evaluate(model, corpus, criterion):
-    
-    model.eval()
-    epoch_loss = 0
-    sentence_num = 0
+	
+	model.eval()
+	epoch_loss = 0
+	sentence_num = 0
 
-    # result from all dev sentences, both idx form and literal form
-    all_definitions = []
-    all_sentence_result = []
-    
-    with torch.no_grad():
-    
-        for sub_corpus in corpus[small_train_size:small_dev_size]:
-    
-            for sent in sub_corpus:
-                
-                sentence = [word.text for word in sent]
-                
-                # get the tagged ambiguous words
-                tagged_sent = [instance for instance in sent if instance.tag == 'instance']
-                # print(sentence)
-                # print(tagged_sent)
+	# result from all dev sentences, both idx form and literal form
+	all_definitions = []
+	all_sentence_result = []
+	
+	with torch.no_grad():
+	
+		for sub_corpus in corpus[0:small_dev_size]:
+	
+			for sent in sub_corpus:
+				
+				sentence = [word.text for word in sent]
+				
+				# get the tagged ambiguous words
+				tagged_sent = [instance for instance in sent if instance.tag == 'instance']
+				# print(sentence)
+				# print(tagged_sent)
 
-                # only use sentence with at least one tagged word
-                if len(tagged_sent) > 0:
-                    sentence_num += 1
+				# only use sentence with at least one tagged word
+				if len(tagged_sent) > 0:
+					sentence_num += 1
 
-                    # get all-word definitions, batch_size is the sentence length
-                    # [batch_size, self.max_length]
-                    definitions = []
-                    literal_def = []
-                    for instance in tagged_sent:
+					# get all-word definitions, batch_size is the sentence length
+					# [batch_size, self.max_length]
+					definitions = []
+					literal_def = []
+					for instance in tagged_sent:
 
-                        # get the sense from the target file with ID
-                        definition = get_def(instance)
-                        def_idx_list = def2idx(definition, model.max_length, vocab)
-                        definitions.append(def_idx_list)
-                        literal_def.append(definition)
+						# get the sense from the target file with ID
+						definition = get_SemEval_def(instance)
+						def_idx_list = def2idx(definition, model.max_length, vocab)
+						definitions.append(def_idx_list)
+						literal_def.append(definition)
 
-                    # get the encoder-decoder result
-                    # (self.max_length, batch_size, vocab_size)
-                    output, result = model(sentence, tagged_sent, definitions, teacher_forcing_ratio = 0)
-                    all_sentence_result.append(result)
-                    all_definitions.append(literal_def)
-                    
-                    # adjust dimension for loss calculation
-                    # (self.max_length * batch_size, vocab_size)
-                    output = output.view(-1, output.shape[-1])
-                    target = torch.tensor(definitions, dtype = torch.long).to(device)
-                    # (self.max_length * batch_size)
-                    target = torch.transpose(target, 0, 1).contiguous().view(-1)
-                    '''
-                    output = output.permute(1, 2, 0)
-                    target = torch.tensor(definitions, dtype = torch.long).to(device)
-                    '''
-                    loss = criterion(output, target)        
-                    epoch_loss += loss.item()
-                    
-    return epoch_loss / sentence_num, all_sentence_result, all_definitions
+					# get the encoder-decoder result
+					# (self.max_length, batch_size, vocab_size)
+					output, result = model(sentence, tagged_sent, definitions, teacher_forcing_ratio = 0)
+					all_sentence_result.append(result)
+					all_definitions.append(literal_def)
+					
+					# adjust dimension for loss calculation
+					# (self.max_length * batch_size, vocab_size)
+					output = output.view(-1, output.shape[-1])
+					target = torch.tensor(definitions, dtype = torch.long).to(device)
+					# (self.max_length * batch_size)
+					target = torch.transpose(target, 0, 1).contiguous().view(-1)
+					'''
+					output = output.permute(1, 2, 0)
+					target = torch.tensor(definitions, dtype = torch.long).to(device)
+					'''
+					loss = criterion(output, target)        
+					epoch_loss += loss.item()
+					
+	return epoch_loss / sentence_num, all_sentence_result, all_definitions
 
 
 # In[11]:
@@ -292,10 +309,10 @@ def evaluate(model, corpus, criterion):
 
 # time used by each epoch
 def epoch_time(start_time, end_time):
-    elapsed_time = end_time - start_time
-    elapsed_mins = int(elapsed_time / 60)
-    elapsed_secs = int(elapsed_time - (elapsed_mins * 60))
-    return elapsed_mins, elapsed_secs
+	elapsed_time = end_time - start_time
+	elapsed_mins = int(elapsed_time / 60)
+	elapsed_secs = int(elapsed_time - (elapsed_mins * 60))
+	return elapsed_mins, elapsed_secs
 
 
 # In[12]:
@@ -304,17 +321,17 @@ def epoch_time(start_time, end_time):
 # utility function
 # arrange the result back to literal readable form
 def arrange_result(all_sentence_result):
-    arranged_all_sentence_result = []
-    for result in all_sentence_result:
-        arranged_results = []
-        for n in range(len(result[0])):
-            sense = ''
-            for m in range(len(result)):
-                w = ' '+ vocab.idx2word.get(int(result[m][n]))
-                sense += w
-            arranged_results.append(sense)
-        arranged_all_sentence_result.append(arranged_results)
-    return arranged_all_sentence_result
+	arranged_all_sentence_result = []
+	for result in all_sentence_result:
+		arranged_results = []
+		for n in range(len(result[0])):
+			sense = ''
+			for m in range(len(result)):
+				w = ' '+ vocab.idx2word.get(int(result[m][n]))
+				sense += w
+			arranged_results.append(sense)
+		arranged_all_sentence_result.append(arranged_results)
+	return arranged_all_sentence_result
 
 
 # In[13]:
@@ -323,16 +340,16 @@ def arrange_result(all_sentence_result):
 # utility function
 # write the results to the file, with the ground-truth
 def write_result_to_file(arranged_all_sentence_result, all_definition):
-    with open('result.txt', 'w') as f:
-        for idx, arranged_results in enumerate(arranged_all_sentence_result):
-            f.write("sentence {}\n".format(idx))
+	with open('result.txt', 'w') as f:
+		for idx, arranged_results in enumerate(arranged_all_sentence_result):
+			f.write("sentence {}\n".format(idx))
 
-            for literal_def in all_definitions[idx]:
-                f.write("%s\n" % literal_def)
+			for literal_def in all_definitions[idx]:
+				f.write("%s\n" % literal_def)
 
-            for item in arranged_results:
-                f.write("%s\n" % item)
-            f.write("\n")
+			for item in arranged_results:
+				f.write("%s\n" % item)
+			f.write("\n")
 
 
 # In[14]:
@@ -348,33 +365,33 @@ train_losses = []
 dev_losses = []
 
 for epoch in range(N_EPOCHS):
-    
-    start_time = time.time()
-    
-    train_loss = train(emb2seq_model, optimizer, corpus, criterion, CLIP)
-    train_losses.append(train_loss)
-    
-    valid_loss, all_sentence_result, all_definitions = evaluate(emb2seq_model, corpus, criterion)
-    dev_losses.append(valid_loss)
-        
-    end_time = time.time()
-    epoch_mins, epoch_secs = epoch_time(start_time, end_time)
-    
-    # visualize the results
-    arranged_all_sentence_result = arrange_result(all_sentence_result)
-            
-    # save the best model based on the dev set
-    if valid_loss <= best_valid_loss:
+	
+	start_time = time.time()
+	
+	train_loss = train(emb2seq_model, optimizer, semcor_corpus, criterion, CLIP)
+	train_losses.append(train_loss)
+	
+	valid_loss, all_sentence_result, all_definitions = evaluate(emb2seq_model, semeval_corpus, criterion)
+	dev_losses.append(valid_loss)
+		
+	end_time = time.time()
+	epoch_mins, epoch_secs = epoch_time(start_time, end_time)
+	
+	# visualize the results
+	arranged_all_sentence_result = arrange_result(all_sentence_result)
+			
+	# save the best model based on the dev set
+	if valid_loss <= best_valid_loss:
 
-        best_valid_loss = valid_loss
-        torch.save(emb2seq_model.state_dict(), 'best_model.pth')
-        
-        # record the result
-        write_result_to_file(arranged_all_sentence_result, all_definitions)
-    
-    print(f'Epoch: {epoch+1:02} | Time: {epoch_mins}m {epoch_secs}s')
-    print(f'\tTrain Loss: {train_loss:.3f} | Train PPL: {math.exp(train_loss):7.3f}')
-    print(f'\t Val. Loss: {valid_loss:.3f} |  Val. PPL: {math.exp(valid_loss):7.3f}')
+		best_valid_loss = valid_loss
+		torch.save(emb2seq_model.state_dict(), 'emb2seq_best_model.pth')
+		
+		# record the result
+		write_result_to_file(arranged_all_sentence_result, all_definitions)
+	
+	print(f'Epoch: {epoch+1:02} | Time: {epoch_mins}m {epoch_secs}s')
+	print(f'\tTrain Loss: {train_loss:.3f} | Train PPL: {math.exp(train_loss):7.3f}')
+	print(f'\t Val. Loss: {valid_loss:.3f} |  Val. PPL: {math.exp(valid_loss):7.3f}')
 
 
 # In[15]:
@@ -386,12 +403,12 @@ import matplotlib.pyplot as plt
 from matplotlib import rc
 
 with open('train_loss.tsv', mode = 'w') as loss_file:
-    csv_writer = csv.writer(loss_file)
-    csv_writer.writerow(train_losses)
+	csv_writer = csv.writer(loss_file)
+	csv_writer.writerow(train_losses)
 
 with open('dev_loss.tsv', mode = 'w') as loss_file: 
-    csv_writer = csv.writer(loss_file)
-    csv_writer.writerow(dev_losses)
+	csv_writer = csv.writer(loss_file)
+	csv_writer.writerow(dev_losses)
 
 
 # In[16]:

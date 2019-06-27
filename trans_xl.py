fbc9df93b01151797b6ce1788ce1e844fe7f8feb
cristianoBY <boyu2844240635@gmail.com>
2019-06-27 18:25:50 -0400

    tuned transformer-xl

--- trans_xl.py
+++ trans_xl.py
@@ -18,23 +18,25 @@ text_1 = "what do we need to make"
 text_2 = "in order to make it or to"
 tokenized_text_1 = tokenizer.tokenize(text_1)
 tokenized_text_2 = tokenizer.tokenize(text_2)
-print(tokenized_text_1, tokenized_text_2)
+# print(tokenized_text_1, tokenized_text_2)
 
 # get the decoder vocab
 with open('./data/vocab.pkl', 'rb') as f:
 	vocab = pickle.load(f)
 	print("Size of vocab: {}".format(vocab.idx))
 
-words_in_order = [vocab.idx2word.get(idx) for idx in range(vocab.idx)]
-# print(words_in_order)
+word_idx_in_order = [tokenizer.convert_tokens_to_ids([vocab.idx2word.get(idx)])[0] for idx in range(vocab.idx)]
+print(len(word_idx_in_order))
 
 # see how many unk words are there
+'''
 num = []
 for key, value in vocab.idx2word.items():
 	if tokenizer.convert_tokens_to_ids([value]) == [24]:
 		num.append(value)
 print(num)
-# print(len(num))
+print(len(num))
+'''
 
 # print(tokenizer.convert_ids_to_tokens([24]))
 # Convert token to vocabulary indices
@@ -58,12 +60,13 @@ with torch.no_grad():
     # Predict all tokens
     predictions_1, mems_1 = model(tokens_tensor_1)
     # We can re-use the memory cells in a subsequent call to attend a longer context
-    predictions_2, mems_2 = model(tokens_tensor_2, mems=mems_1)
+    predictions_2, mems_2 = model(tokens_tensor_2, mems = mems_1)
 
 # get the predicted last token
 print(predictions_2[0, -1, :].shape)
 predicted_index = torch.argmax(predictions_2[0, -1, :]).item()
 predicted_token = tokenizer.convert_ids_to_tokens([predicted_index])[0]
 print(predicted_token)
-assert predicted_token == 'who'
+
+# get subset vocab for our model
 
